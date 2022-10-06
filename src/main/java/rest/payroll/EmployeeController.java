ea548eff7fab5044df4745795c15b88b0ed78c32
Jade Kim <JadeKim.dev@gmail.com>
2022-10-07 00:35:46 +0900

    WebMvcLinkBuilder, Spring HATEOAS

--- src/main/java/rest/payroll/EmployeeController.java
+++ src/main/java/rest/payroll/EmployeeController.java
@@ -1,8 +1,14 @@
 package rest.payroll;
 
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 // To wrap the current repository with a web layer -> Spring MVC
 
@@ -10,46 +16,97 @@ import java.util.List;
 @RestController
 class EmployeeController {
 
-    private final EmployeeRepository respository;
+    private final EmployeeRepository repository;
+
+    private final EmployeeModelAssembler assembler;
 
     // injected
-    EmployeeController(EmployeeRepository respository) {
-        this.respository = respository;
+    public EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
+        this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return respository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+//        return repository.findAll();
+
+        /*
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(employee -> EntityModel.of(employee,
+                        linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                        linkTo(methodOn(EmployeeController.class).all()).withRel("employees")))
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees, linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
+         */
+
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+                linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return respository.findById(id)
+    EntityModel one(@PathVariable Long id) {
+
+        /*
+        return repository.findById(id)
                 .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+        A critical ingredient to any RESTful service is adding links to relevant operations
+
+        EntityModel: a generic container from Spring HATEOAS that includes the data and links
+
+        {
+            "id": 1,
+                "name": "Bilbo Baggins",
+                "role": "burglar",
+                "_links": {
+            "self": {
+                "href": "http://localhost:8080/employees/1"
+            },
+            "employees": {
+                "href": "http://localhost:8080/employees"
+            }
+        }
+        }
+        */
+
+        Employee employee = repository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+//        return EntityModel.of(employee,
+//                linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+//                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+
+        // using the assembler
+        return assembler.toModel(employee);
     }
 
     @PostMapping("/employees")
     Employee newEmployee(@RequestBody Employee newEmployee) {
-        return respository.save(newEmployee);
+        return repository.save(newEmployee);
     }
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
-        return respository.findById(id)
+        return repository.findById(id)
                 .map(employee -> {
                     employee.setName(newEmployee.getName());
                     employee.setId(newEmployee.getId());
-                    return respository.save(employee);
+                    return repository.save(employee);
                 })
                 .orElseGet(() -> {
                     newEmployee.setId(id);
-                    return respository.save(newEmployee);
+                    return repository.save(newEmployee);
                 });
     }
 
     @DeleteMapping("/employees/{id}")
     void deleteEmployee(@PathVariable Long id) {
-        respository.deleteById(id);
+        repository.deleteById(id);
     }
 }
 
---
+++ src/main/java/rest/payroll/EmployeeModelAssembler.java
@@ -0,0 +1,22 @@
+package rest.payroll;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+// Define a function that converts Employee -> EntityModel<Employee> to simplify link creation
+
+public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
+
+    @Override
+    public EntityModel<Employee> toModel(Employee employee) {
+
+        // non-model objects -> model-based objects
+
+        return EntityModel.of(employee,
+                linkTo(methodOn(EmployeeController.class).one(employee.getId())).withSelfRel(),
+                linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    }
+}
