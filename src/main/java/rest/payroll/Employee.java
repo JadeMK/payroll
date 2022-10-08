e870890dc72ffd5af6113d6624f2f558fbfce80e
Jade Kim <JadeKim.dev@gmail.com>
2022-10-08 17:11:44 +0900

    Evolving REST APIs

--- src/main/java/rest/payroll/Employee.java
+++ src/main/java/rest/payroll/Employee.java
@@ -14,15 +14,19 @@ class Employee {
     private Long id;
     // indicates that it is the primary key & automatically populated by the JPA provider
 
-    private String name;
+    // Upgrade the application to support first & last names
+    // private String name;
 
+    private String firstName;
+    private String lastName;
     private String role;
 
     public Employee() {
     }
 
-    public Employee(String name, String role) {
-        this.name = name;
+    public Employee(String firstName, String lastName, String role) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.role = role;
     }
 
@@ -35,11 +39,15 @@ class Employee {
     }
 
     public String getName() {
-        return name;
+        // name -> first name + last name
+        return this.firstName + " " + this.lastName;
     }
 
     public void setName(String name) {
-        this.name = name;
+        // implement the name updates
+        String[] parts = name.split(" ");
+        this.firstName = parts[0];
+        this.lastName = parts[1];
     }
 
     public String getRole() {
@@ -48,6 +56,22 @@ class Employee {
 
     public void setRole(String role) {
         this.role = role;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 
     @Override
@@ -59,17 +83,19 @@ class Employee {
             return false;
         }
         Employee employee = (Employee) o;
-        return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
+        return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+                && Objects.equals(this.lastName, employee.lastName)
                 && Objects.equals(this.role, employee.role);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(this.id, this.name, this.role);
+        return Objects.hash(this.id, this.firstName, this.lastName, this.role);
     }
 
     @Override
     public String toString() {
-        return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+        return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' +
+                ", lastName='" + this.lastName + '\'' + ", role='" + this.role + '\'' + '}';
     }
 }
--- src/main/java/rest/payroll/EmployeeModelAssembler.java
+++ src/main/java/rest/payroll/EmployeeModelAssembler.java
@@ -2,12 +2,14 @@ package rest.payroll;
 
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 // Define a function that converts Employee -> EntityModel<Employee> to simplify link creation
 
+@Component
 public class EmployeeModelAssembler implements RepresentationModelAssembler<Employee, EntityModel<Employee>> {
 
     @Override
